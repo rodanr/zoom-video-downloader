6629fa32a7989f9fceac216e9681f031d6801c98
rodan0818 <rodanramdam0818@gmail.com>
2021-07-12 19:55:08 +0545

    Now files are downloaded to Dowloads directory and downloading animation added and other bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron");
 const { ipcMain } = require("electron");
 const { execFile } = require("child_process");
+const os = require("os")
+var downloadDir = os.homedir() + `\\Downloads`
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -15,9 +17,11 @@ function createWindow() {
   win.loadFile("index.html");
   win.setMenu(null);
   win.setIcon("src/title-bar-icon.png");
+  win.webContents.openDevTools()
 }
 app.whenReady().then(() => {
   createWindow();
+  console.log(downloadDir);
 });
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
@@ -26,22 +30,23 @@ ipcMain.on("data", (event, data) => {
   let url = data["url"].trim();
   let password = data["password"].trim();
   if (password == "") {
-    execFile("libs/youtube-dl.exe", [`${url}`], (error, stdout, stderr) => {
+    execFile("libs/youtube-dl.exe", [`${url}`, "--output", downloadDir + '\\ZoomDownloads\\%(title)s.%(ext)s'], (error, stdout, stderr) => {
       if (error) {
         throw error;
       }
       console.log();
-      event.sender.send("output", stdout);
+      // event.sender.send("output", stdout);
+      event.sender.send("output", "Your file is downloaded at " + downloadDir + "\\ZoomDownloads")
     });
   } else {
     execFile(
       "libs/youtube-dl.exe",
-      [`${url}`, "--video-password", `${password}`],
+      [`${url}`, "--video-password", `${password}`, "-o", downloadDir],
       (error, stdout, stderr) => {
         if (error) {
           throw error;
         }
-        event.sender.send("output", stdout);
+        event.sender.send("output", "Your file is downloaded at " + downloadDir + "\\ZoomDownloads");
       }
     );
   }
